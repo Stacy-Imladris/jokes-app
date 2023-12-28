bfa3fa8854a427bb29e00b3f8af260abb221948c
stacy <ada.davis27@gmail.com>
2023-12-28 18:13:26 +0300

    added joke

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,7 @@
 import {PropsWithChildren} from 'react';
 import s from './Container.module.css'
 
-export const Container = ({children}: PropsWithChildren) => (
+export const Container = ({children}: PropsWithChildren<{}>) => (
     <div className={s.wrapper}>
         <div className={s.content}>{children}</div>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {App} from 'components/App/App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import {App} from 'components/App/App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <HashRouter>
